43fa375527a9bde9acd2df0f194f5557909f450d
adrianrodriguez09 <103323248+adrianrodriguez09@users.noreply.github.com>
2022-12-19 22:57:29 -0300

    cambios finales

--- js(prin)/stock.js
+++ js(prin)/stock.js
@@ -1,8 +1,8 @@
 let stockProductos = [
-    {id: 1, nombre: "habitacion Matrimonial (1)", tipo: "Para 2 personas comoda", cantidad: 1, desc: "es una habitacion que cumple con todo y mas.....", precio:39700, img: './img/habi1.jpg'},
-    {id: 2, nombre: "habitacion matrimonial (2)", tipo: "cara", cantidad: 1, desc: "es una habitacion que cumple con todo y mas.....", precio: 39700, img: './img/habi2.jpg'},
-    {id: 3, nombre: "habitacion Single", tipo: "para una sola persona", cantidad: 1, desc: "es una habitacion que cumple con todo y mas.....", precio: 31720, img: './img/habi3.jpg'},
-    {id: 4, nombre: "habitacion Single", tipo: "para una sola persona", cantidad: 1, desc: "es una habitacion que cumple con todo y mas.....", precio: 31720, img: './img/habi4.jpg'},
+    {id: 1, nombre: "habitacion Leon", tipo: "Para 2 personas comoda", cantidad: 1, desc: "es una habitacion que cumple con todo y mas.....", precio:39700, img: './img/habi1.jpg'},
+    {id: 2, nombre: "habitacion Flor de Genaro", tipo: "cara", cantidad: 1, desc: "es una habitacion que cumple con todo y mas.....", precio: 39700, img: './img/habi2.jpg'},
+    {id: 3, nombre: "habitacion Mariposa", tipo: "para una sola persona", cantidad: 1, desc: "es una habitacion que cumple con todo y mas.....", precio: 31720, img: './img/habi3.jpg'},
+    {id: 4, nombre: "habitacion Colibri", tipo: "para una sola persona", cantidad: 1, desc: "es una habitacion que cumple con todo y mas.....", precio: 31720, img: './img/habi4.jpg'},
 
     
 ]
